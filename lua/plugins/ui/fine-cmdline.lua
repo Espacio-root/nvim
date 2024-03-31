06c4a293c18d08da6a131975d45f6b1ab0acb1eb
Espacio-root <abdullahd00124@gmail.com>
2024-04-01 04:35:27 +0530

    updated ui

--- lua/plugins/ui/fine-cmdline.lua
+++ lua/plugins/ui/fine-cmdline.lua
@@ -1,8 +1,32 @@
+local custom = require("customization")
+
 return {
   'VonHeikemen/fine-cmdline.nvim',
   enabled = false,
   dependencies = {
     'MunifTanjim/nui.nvim'
+  },
+  opts = {
+    cmdline = {
+      enable_keymaps = true,
+      smart_history = true,
+      prompt = ': '
+    },
+    popup = {
+      position = {
+        row = '10%',
+        col = '50%',
+      },
+      size = {
+        width = '60%',
+      },
+      border = {
+        style = custom.border,
+      },
+      win_options = {
+        winhighlight = 'Normal:Normal,FloatBorder:FloatBorder',
+      },
+    },
   },
   keys = {
     -- {
