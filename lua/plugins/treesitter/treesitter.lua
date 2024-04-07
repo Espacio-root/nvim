49f2ed9b19f8a8306ceebb15b366ef1a5d22a73e
Espacio-root <abdullahd00124@gmail.com>
2024-04-08 02:10:36 +0530

    minor change: moved treesitter to its own directory

---
+++ lua/plugins/treesitter/treesitter.lua
@@ -0,0 +1,31 @@
+local function disable_treesitter(lang, buf)
+  local max_filesize = 100 * 1024 -- 100 KB
+  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+  if ok and stats and stats.size > max_filesize then
+      return true
+  end
+end
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = "all",
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        disable = disable_treesitter,
+      },
+      indent = {
+        enable = true,
+        disable = disable_treesitter,
+      },
+      endwise = {
+        enable = true,
+        disable = disable_treesitter,
+      },
+    }
+  end,
+}
