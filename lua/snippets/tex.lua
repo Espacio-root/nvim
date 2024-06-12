cb5aa598e2058c15c84a79174fb0981bb4746a08
Espacio-root <abdullahd00124@gmail.com>
2024-06-12 04:43:31 +0300

    defined some basic snippets, ref: https://castel.dev/post/lecture-notes-1/

--- lua/snippets/tex.lua
+++ lua/snippets/tex.lua
@@ -183,7 +183,88 @@ local frac3 = s(
     })
 )
 
+local sympy1 = s( -- This snippets creates the sympy block ;)
+    { trig = "sympy", desc = "Creates a sympy block", snippetType = "autosnippet"},
+    fmt("sympy {} sympy{}",
+        { i(1), i(0) }
+    )
+)
 
+local sympy2 = s( -- This one evaluates anything inside the simpy block
+    { trig = "sympy.*sympy ", wordTrig=false, regTrig = true, desc = "Sympy block evaluator", snippetType = "autosnippet", priority = 10000 },
+    d(1, function(_, parent)
+        -- Gets the part of the block we actually want, and replaces spaces
+        -- at the beginning and at the end
+        local to_eval = string.gsub(parent.trigger, "^sympy(.*)sympy", "%1")
+        to_eval = string.gsub(to_eval, "^%s+(.*)%s+$", "%1")
+
+        local Job = require("plenary.job")
+
+        local sympy_script = string.format(
+            [[
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
+from sympy.printing.latex import print_latex
+parsed = parse_expr('%s')
+print_latex(parsed)
+            ]],
+            to_eval
+        )
+
+        sympy_script = string.gsub(sympy_script, "^[\t%s]+", "")
+        local result = ""
+
+        Job:new({
+            command = "python",
+            args = {
+                "-c",
+                sympy_script,
+            },
+            on_exit = function(j)
+                result = j:result()
+            end,
+        }):sync()
+
+        return sn(nil, t(result))
+    end)
+)
+
+-- postfix snippets
+local bar1 = s(
+  { trig = "bar", dsrc = "bar", snippetType = "autosnippet", priority = 10 },
+  fmta([[
+  \overline{<content>}<after>
+  ]], {content = i(1), after = i(0)})
+)
+
+local bar2 = s(
+  { trig = "([a-zA-Z])bar", dsrc = "bar with letter", regTrig = true, snippetType = "autosnippet", priority = 100 },
+  fmta([[
+  \overline{<content>}<after>
+  ]], {content = f(function(_,snip) return snip.captures[1] end), after = i(0)})
+)
+
+
+local hat1 = s(
+  { trig = "hat", dsrc = "hat", snippetType = "autosnippet", priority = 10 },
+  fmta([[
+  \hat{<content>}<after>
+  ]], {content = i(1), after = i(0)})
+)
+
+local hat2 = s(
+  { trig = "([a-zA-Z])hat", dsrc = "hat with letter", regTrig = true, snippetType = "autosnippet", priority = 100 },
+  fmta([[
+  \hat{<content>}<after>
+  ]], {content = f(function(_,snip) return snip.captures[1] end), after = i(0)})
+)
+
+local vec = s(
+  {trig = [[(\\?\w+)(,\.|\.,)]], dsrc = "vector", trigEngine = "ecma", snippetType = "autosnippet"},
+  fmta([[
+  \vec{<content>}<after>
+  ]], {content = f(function(_,snip) return snip.captures[1] end), after = i(0)})
+)
 
 
 return {
@@ -191,5 +272,7 @@ return {
   mk, dm,
   sub1, sub2,
   sr, cb, compl, td,
-  frac1, frac2, frac3
+  frac1, frac2, frac3,
+  sympy1, sympy2,
+  bar1, bar2, hat1, hat2, vec
 }
